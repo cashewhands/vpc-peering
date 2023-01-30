c5b55a2c0fb07eac5f7fc78f069ffa40dd476dc0
Demola <demoladev.ops@gmail.com>
2023-01-30 14:57:38 +0000

    Chore:vpc-trial

--- configs/development.tfvars
+++ configs/development.tfvars
@@ -1,4 +1,4 @@
-environment = "development"
-requestor_vpc_id    = "vpc-0583bbbbd80c89ed6"
-acceptor_vpc_id     = "vpc-0c38e27563088c666"
-region      = "us-east-1"
+environment      = "development"
+requestor_vpc_id = "vpc-0583bbbbd80c89ed6"
+acceptor_vpc_id  = "vpc-0c38e27563088c666"
+region           = "us-east-1"
--- data.tf
+++ data.tf
@@ -1,13 +1,13 @@
 # Lookup requestor VPC so that we can reference the CIDR
 data "aws_vpc" "requestor" {
   /* count = var.is_enabled ? 1 : 0 */
-  id    = var.requestor_vpc_id
+  id = var.requestor_vpc_id
 }
 
 # Lookup acceptor VPC so that we can reference the CIDR
 data "aws_vpc" "acceptor" {
   /* count = var.is_enabled ? 1 : 0 */
-  id    = var.acceptor_vpc_id
+  id = var.acceptor_vpc_id
 }
 
 data "aws_route_tables" "requestor" {
