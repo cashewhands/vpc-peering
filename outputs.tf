78ae925819ceac3815a4efc58b6d46a61b3c62df
Demola <demoladev.ops@gmail.com>
2023-01-30 14:44:24 +0000

    Chore:vpc-trial

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "instance_ip" {
+/* output "instance_ip" {
   value = module.web-server.ec2_public_ip
-}
+} */
 
-output "public_subnet_id" {
+/* output "public_subnet_id" {
   description = "The ID of the subnet"
   value       = module.network-us.public_subnet_id
-}
+} */
 
 /* output "public_subnet_cidr_block" {
   description = "The CIDR Block of the public subnet"
